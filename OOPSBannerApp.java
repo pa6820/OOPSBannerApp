c8f0015e5bce2d45588a297da9b9a69d88eef8e4
Pranav <pa6820@srmist.edu.in>
2026-04-10 05:02:38 +0530

    UC1: Print OOPS implemented

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp - UC1
+ * Prints "OOPS"
+ * 
+ * @author Pranav
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
