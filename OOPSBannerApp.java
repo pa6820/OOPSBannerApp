cecfd1ce8fbc7c96162d6d3b063382bc864bed13
Pranav <pa6820@srmist.edu.in>
2026-04-10 05:32:46 +0530

    UC4: OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,28 @@
 /**
- * OOPSBannerApp - UC1
- * Prints "OOPS"
+ * OOPSBannerApp
+ * UC4: Prints "OOPS" banner using Array & Loop
  * 
  * @author Pranav
- * @version 1.0
+ * @version 4.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] banner = {
+
+            String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
+            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *"),
+            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *"),
+            String.join("", "*     *", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
+            String.join("", "*     *", "  ", "*      ", "  ", "     * ", "  ", "*      "),
+            String.join("", "*     *", "  ", "*      ", "  ", "     * ", "  ", "*      "),
+            String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", "*      ")
+
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
